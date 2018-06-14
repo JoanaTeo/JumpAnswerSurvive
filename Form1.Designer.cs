042cd4a1095d50b9e0162178bbcee78ef7dceea5
jovanadimovska <36921691+jovanadimovska@users.noreply.github.com>
2018-06-14 14:54:50 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace JAS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.aligator = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pochniNivo2 = new System.Windows.Forms.PictureBox();
+            this.prasanje5 = new System.Windows.Forms.PictureBox();
+            this.prasanje1 = new System.Windows.Forms.PictureBox();
+            this.prasanje2 = new System.Windows.Forms.PictureBox();
+            this.prasanje4 = new System.Windows.Forms.PictureBox();
+            this.prasanje6 = new System.Windows.Forms.PictureBox();
+            this.prasanje7 = new System.Windows.Forms.PictureBox();
+            this.prasanje3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aligator)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pochniNivo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox5.Location = new System.Drawing.Point(267, 363);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(74, 15);
+            this.pictureBox5.TabIndex = 16;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "platform";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(176, 313);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(74, 15);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "platform";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox2.Location = new System.Drawing.Point(267, 237);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(74, 15);
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "platform";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox3.Location = new System.Drawing.Point(140, 188);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(74, 15);
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "platform";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox6.Location = new System.Drawing.Point(140, 78);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(74, 15);
+            this.pictureBox6.TabIndex = 21;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "platform";
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox7.Location = new System.Drawing.Point(12, 131);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(74, 15);
+            this.pictureBox7.TabIndex = 22;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "platform";
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox8.Location = new System.Drawing.Point(12, 29);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(74, 15);
+            this.pictureBox8.TabIndex = 23;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "platform";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox4.Location = new System.Drawing.Point(267, 131);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(74, 15);
+            this.pictureBox4.TabIndex = 24;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "platform";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.PaleGreen;
+            this.pictureBox9.Location = new System.Drawing.Point(267, 29);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(74, 15);
+            this.pictureBox9.TabIndex = 25;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "platform";
+            // 
+            // aligator
+            // 
+            this.aligator.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.aligator.Location = new System.Drawing.Point(294, 329);
+            this.aligator.Name = "aligator";
+            this.aligator.Size = new System.Drawing.Size(24, 34);
+            this.aligator.TabIndex = 26;
+            this.aligator.TabStop = false;
+            this.aligator.Tag = "aligator";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pochniNivo2
+            // 
+            this.pochniNivo2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pochniNivo2.Location = new System.Drawing.Point(12, 0);
+            this.pochniNivo2.Name = "pochniNivo2";
+            this.pochniNivo2.Size = new System.Drawing.Size(38, 31);
+            this.pochniNivo2.TabIndex = 27;
+            this.pochniNivo2.TabStop = false;
+            // 
+            // prasanje5
+            // 
+            this.prasanje5.Location = new System.Drawing.Point(141, 62);
+            this.prasanje5.Name = "prasanje5";
+            this.prasanje5.Size = new System.Drawing.Size(73, 10);
+            this.prasanje5.TabIndex = 31;
+            this.prasanje5.TabStop = false;
+            this.prasanje5.Tag = "prasanje";
+            // 
+            // prasanje1
+            // 
+            this.prasanje1.Location = new System.Drawing.Point(175, 297);
+            this.prasanje1.Name = "prasanje1";
+            this.prasanje1.Size = new System.Drawing.Size(73, 10);
+            this.prasanje1.TabIndex = 32;
+            this.prasanje1.TabStop = false;
+            this.prasanje1.Tag = "prasanje";
+            // 
+            // prasanje2
+            // 
+            this.prasanje2.Location = new System.Drawing.Point(268, 221);
+            this.prasanje2.Name = "prasanje2";
+            this.prasanje2.Size = new System.Drawing.Size(73, 10);
+            this.prasanje2.TabIndex = 33;
+            this.prasanje2.TabStop = false;
+            this.prasanje2.Tag = "prasanje";
+            // 
+            // prasanje4
+            // 
+            this.prasanje4.Location = new System.Drawing.Point(141, 172);
+            this.prasanje4.Name = "prasanje4";
+            this.prasanje4.Size = new System.Drawing.Size(73, 10);
+            this.prasanje4.TabIndex = 34;
+            this.prasanje4.TabStop = false;
+            this.prasanje4.Tag = "prasanje";
+            // 
+            // prasanje6
+            // 
+            this.prasanje6.Location = new System.Drawing.Point(12, 115);
+            this.prasanje6.Name = "prasanje6";
+            this.prasanje6.Size = new System.Drawing.Size(73, 10);
+            this.prasanje6.TabIndex = 35;
+            this.prasanje6.TabStop = false;
+            this.prasanje6.Tag = "prasanje";
+            // 
+            // prasanje7
+            // 
+            this.prasanje7.Location = new System.Drawing.Point(267, 13);
+            this.prasanje7.Name = "prasanje7";
+            this.prasanje7.Size = new System.Drawing.Size(73, 10);
+            this.prasanje7.TabIndex = 36;
+            this.prasanje7.TabStop = false;
+            this.prasanje7.Tag = "prasanje";
+            // 
+            // prasanje3
+            // 
+            this.prasanje3.Location = new System.Drawing.Point(268, 115);
+            this.prasanje3.Name = "prasanje3";
+            this.prasanje3.Size = new System.Drawing.Size(73, 10);
+            this.prasanje3.TabIndex = 37;
+            this.prasanje3.TabStop = false;
+            this.prasanje3.Tag = "prasanje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::JAS.Properties.Resources.ACF31;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(374, 495);
+            this.Controls.Add(this.prasanje3);
+            this.Controls.Add(this.prasanje7);
+            this.Controls.Add(this.prasanje6);
+            this.Controls.Add(this.prasanje4);
+            this.Controls.Add(this.prasanje2);
+            this.Controls.Add(this.prasanje1);
+            this.Controls.Add(this.prasanje5);
+            this.Controls.Add(this.pochniNivo2);
+            this.Controls.Add(this.aligator);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aligator)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pochniNivo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prasanje3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox aligator;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pochniNivo2;
+        private System.Windows.Forms.PictureBox prasanje5;
+        private System.Windows.Forms.PictureBox prasanje1;
+        private System.Windows.Forms.PictureBox prasanje2;
+        private System.Windows.Forms.PictureBox prasanje4;
+        private System.Windows.Forms.PictureBox prasanje6;
+        private System.Windows.Forms.PictureBox prasanje7;
+        private System.Windows.Forms.PictureBox prasanje3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace JAS
+{
+    public partial class Form1 : Form
+    {
+        bool naLevo = false;
+        bool naDesno = false;
+        bool skok = false;
+        bool flag = true;
+        int brzinaSkok = 10;
+        int sila = 8;
+        int poeni = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                naLevo = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                naDesno = true;
+            }
+            if (e.KeyCode == Keys.Space && !skok)
+            {
+                skok = true;
+            }
+
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                naLevo = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                naDesno = false;
+            }
+            if (skok)
+            {
+                skok = false;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            aligator.Top += brzinaSkok;
+            if (skok && sila < 0)
+            {
+                skok = false;
+
+            }
+            if (naLevo)
+            {
+                aligator.Left -= 5;
+
+            }
+            if (naDesno)
+            {
+                aligator.Left += 5;
+
+            }
+            if (skok)
+            {
+                brzinaSkok = -12;
+                sila -= 1;
+
+            }
+            else
+            {
+                brzinaSkok = 12;
+            }
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is PictureBox && x.Tag == "platform")
+
+                    if (aligator.Bounds.IntersectsWith(x.Bounds) && !skok)
+
+                    {
+                        sila = 8;
+                        aligator.Top = x.Top - aligator.Height;
+                    }
+                if (x is PictureBox && x.Tag == "prasanje")
+                {
+                    if (aligator.Bounds.IntersectsWith(x.Bounds) && !skok)
+                    {
+                        if(x.Name=="prasanje1")
+                        {
+                            if (flag == true)
+                            { DialogResult dialogResult = MessageBox.Show("Kako si?", "Dobro", MessageBoxButtons.YesNo);
+                                flag = false;
+
+                                if (dialogResult == DialogResult.Yes && flag == false)
+                                    continue;
+                                if (dialogResult == DialogResult.No && flag == false)
+                                    continue;
+                              }  
+                        }
+                    }
+                }
+            }
+           
+            if (aligator.Bounds.IntersectsWith(pochniNivo2.Bounds))
+            {
+                timer1.Stop();
+                MessageBox.Show("Bravo  be Angel!");
+            }
+        }
+
+        
+    }
+}
